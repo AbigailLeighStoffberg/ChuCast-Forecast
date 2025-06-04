3289e5172ad361b811e1b36f3f95b9c0c87b82b0
Abigail-Leigh Stoffberg <abizoide@gmail.com>
2025-06-04 17:48:04 +0300

    Added search functionality using the Api

--- src/app.js
+++ src/app.js
@@ -1,10 +1,26 @@
+function refreshWeather(response) {
+let temperatureElement = document.querySelector("#temperature");
+let temperature = response.data.temperature.current;
+let cityElement = document.querySelector("#city");
+
+cityElement.innerHTML = response.data.city;
+temperatureElement.innerHTML = Math.round(temperature);  
+}
+
+function searchCity(city) {
+    let apiKey= "o0e08e5bc5b0e4ff55a41bb73c22t77e";
+    let apiUrl= `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event){
 event.preventDefault();
 let searchInput = document.querySelector("#search-input");
-let cityElement = document.querySelector("#city");
-cityElement.innerHTML = searchInput.value;
+searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#weather-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Sunnyvale");
 
